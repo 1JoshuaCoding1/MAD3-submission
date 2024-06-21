88f660dfe990a76c5ea14157a54c801f0f4de3a3
aldione <aldionejoshua.pancho.21@usjr.edu.ph>
2024-06-21 23:09:00 +0800

    first commit

---
+++ mobile-3-session-1-stateful-management-REST-consumption/lib/src/screens/rest_demo.dart
@@ -0,0 +1,374 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:http/http.dart' as http;
+import 'package:state_change_demo/src/models/post.model.dart';
+import 'package:state_change_demo/src/models/user.model.dart';
+
+class RestDemoScreen extends StatefulWidget {
+  const RestDemoScreen({super.key});
+
+  @override
+  State<RestDemoScreen> createState() => _RestDemoScreenState();
+}
+
+class _RestDemoScreenState extends State<RestDemoScreen> {
+  PostController controller = PostController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.getPosts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Posts"),
+        leading: IconButton(
+            onPressed: () {
+              controller.getPosts();
+            },
+            icon: const Icon(Icons.refresh)),
+        actions: [
+          IconButton(
+              onPressed: () {
+                showNewPostFunction(context);
+              },
+              icon: const Icon(Icons.add))
+        ],
+      ),
+      body: SafeArea(
+        child: ListenableBuilder(
+            listenable: controller,
+            builder: (context, _) {
+              if (controller.error != null) {
+                return Center(
+                  child: Text(controller.error.toString()),
+                );
+              }
+
+              if (!controller.working) {
+                return Center(
+                  child: SingleChildScrollView(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          for (Post post in controller.postList)
+                            GestureDetector(
+                              onTap: () {
+                                showPostDetails(context, post);
+                              },
+                              child: Container(
+                                  padding: const EdgeInsets.all(8),
+                                  margin: const EdgeInsets.only(bottom: 8),
+                                  decoration: BoxDecoration(
+                                      border:
+                                          Border.all(color: Colors.blueAccent),
+                                      borderRadius: BorderRadius.circular(16)),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        post.title,
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      SizedBox(height: 8),
+                                      Text(post.body,
+                                          maxLines: 2,
+                                          overflow: TextOverflow.ellipsis),
+                                    ],
+                                  )),
+                            )
+                        ],
+                      )),
+                );
+              }
+              return const Center(
+                child: SpinKitChasingDots(
+                  size: 54,
+                  color: Colors.black87,
+                ),
+              );
+            }),
+      ),
+    );
+  }
+
+  showNewPostFunction(BuildContext context) {
+    AddPostDialog.show(context, controller: controller);
+  }
+
+  showPostDetails(BuildContext context, Post post) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(post.title),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(post.body),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () async {
+                controller.deletePost(post.id);
+                Navigator.of(context).pop();
+              },
+              child: Text("Delete"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AddPostDialog extends StatefulWidget {
+  static show(BuildContext context, {required PostController controller}) =>
+      showDialog(
+          context: context, builder: (dContext) => AddPostDialog(controller));
+  const AddPostDialog(this.controller, {super.key});
+
+  final PostController controller;
+
+  @override
+  State<AddPostDialog> createState() => _AddPostDialogState();
+}
+
+class _AddPostDialogState extends State<AddPostDialog> {
+  late TextEditingController bodyC, titleC;
+
+  @override
+  void initState() {
+    super.initState();
+    bodyC = TextEditingController();
+    titleC = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+      title: const Text("Add new post"),
+      actions: [
+        ElevatedButton(
+          onPressed: () async {
+            widget.controller.makePost(
+                title: titleC.text.trim(), body: bodyC.text.trim(), userId: 1);
+            Navigator.of(context).pop();
+          },
+          child: const Text("Add"),
+        )
+      ],
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text("Title"),
+          Flexible(
+            child: TextFormField(
+              controller: titleC,
+            ),
+          ),
+          const Text("Content"),
+          Flexible(
+            child: TextFormField(
+              controller: bodyC,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PostController with ChangeNotifier {
+  Map<String, dynamic> posts = {};
+  bool working = true;
+  Object? error;
+
+  List<Post> get postList => posts.values.whereType<Post>().toList();
+
+  clear() {
+    error = null;
+    posts = {};
+    notifyListeners();
+  }
+
+  Future<Post> makePost(
+      {required String title,
+      required String body,
+      required int userId}) async {
+    try {
+      working = true;
+      if (error != null) error = null;
+      http.Response res = await HttpService.post(
+          url: "https://jsonplaceholder.typicode.com/posts",
+          body: {"title": title, "body": body, "userId": userId});
+      if (res.statusCode != 200 && res.statusCode != 201) {
+        throw Exception("${res.statusCode} | ${res.body}");
+      }
+
+      Map<String, dynamic> result = jsonDecode(res.body);
+
+      Post output = Post.fromJson(result);
+      posts[output.id.toString()] = output;
+      working = false;
+      notifyListeners();
+      return output;
+    } catch (e, st) {
+      error = e;
+      working = false;
+      notifyListeners();
+      return Post.empty;
+    }
+  }
+
+  Future<void> getPosts() async {
+    try {
+      working = true;
+      clear();
+      List result = [];
+      http.Response res = await HttpService.get(
+          url: "https://jsonplaceholder.typicode.com/posts");
+      if (res.statusCode != 200 && res.statusCode != 201) {
+        throw Exception("${res.statusCode} | ${res.body}");
+      }
+      result = jsonDecode(res.body);
+
+      List<Post> tmpPost = result.map((e) => Post.fromJson(e)).toList();
+      posts = {for (Post p in tmpPost) "${p.id}": p};
+      working = false;
+      notifyListeners();
+    } catch (e, st) {
+      error = e;
+      working = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> deletePost(int id) async {
+    try {
+      posts.remove(id.toString());
+      notifyListeners();
+    } catch (e, st) {
+      print(e);
+      print(st);
+      error = e;
+      notifyListeners();
+    }
+  }
+}
+
+class UserController with ChangeNotifier {
+  Map<String, dynamic> users = {};
+  bool working = true;
+  Object? error;
+
+  List<User> get userList => users.values.whereType<User>().toList();
+
+  getUsers() async {
+    try {
+      working = true;
+      List result = [];
+      http.Response res = await HttpService.get(
+          url: "https://jsonplaceholder.typicode.com/users");
+      if (res.statusCode != 200 && res.statusCode != 201) {
+        throw Exception("${res.statusCode} | ${res.body}");
+      }
+      result = jsonDecode(res.body);
+
+      List<User> tmpUser = result.map((e) => User.fromJson(e)).toList();
+      users = {for (User u in tmpUser) "${u.id}": u};
+      working = false;
+      notifyListeners();
+    } catch (e, st) {
+      print(e);
+      print(st);
+      error = e;
+      working = false;
+      notifyListeners();
+    }
+  }
+
+  clear() {
+    users = {};
+    notifyListeners();
+  }
+}
+
+class HttpService {
+  static Future<http.Response> get(
+      {required String url, Map<String, dynamic>? headers}) async {
+    Uri uri = Uri.parse(url);
+    return http.get(uri, headers: {
+      'Content-Type': 'application/json',
+      if (headers != null) ...headers
+    });
+  }
+
+  static Future<http.Response> post(
+      {required String url,
+      required Map<dynamic, dynamic> body,
+      Map<String, dynamic>? headers}) async {
+    Uri uri = Uri.parse(url);
+    return http.post(uri, body: jsonEncode(body), headers: {
+      'Content-Type': 'application/json',
+      if (headers != null) ...headers
+    });
+  }
+}
+
+class Post {
+  final int id;
+  final String title;
+  final String body;
+  final int userId;
+
+  Post({required this.id, required this.title, required this.body, required this.userId});
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+      userId: json['userId'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Post{id: $id, title: $title, body: $body, userId: $userId}';
+  }
+
+  static Post empty = Post(id: 0, title: '', body: '', userId: 0);
+}
+
+class User {
+  final int id;
+  final String name;
+  final String username;
+  final String email;
+
+  User({required this.id, required this.name, required this.username, required this.email});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      username: json['username'],
+      email: json['email'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'User{id: $id, name: $name, username: $username, email: $email}';
+  }
+}
